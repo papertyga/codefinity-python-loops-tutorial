39b56ebd96adbe604b285dff905c1415f4987423
papertyga <>
2026-02-17 21:13:02 +0000

    Range iteration

---
+++ the_for_loop/range_iteration/main.py
@@ -0,0 +1,15 @@
+# List of countries
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# List of countries you agreed to visit
+your_travel_list = []
+
+for country in range(0, len(countries), 2):
+    your_travel_list.append(countries[country])
+    
+
+    
+
+
+# Testing
+print(your_travel_list)
