826a2b8c162f1c602f2ea125a1e3ad03ac8d10d0
papertyga <>
2026-02-17 22:27:16 +0000

    While loops

---
+++ the_while_loop/while_loops/main.py
@@ -0,0 +1,13 @@
+# List of countries
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# List to hold the selected countries
+selected = []
+
+country = 0
+while country < len(countries)//2:
+    selected.append(countries[country])
+    country += 1
+
+# Testing
+print('Selected countries:', selected)
