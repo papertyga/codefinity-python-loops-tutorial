72608d4b6bffb6c057422d8a16f9eec03076afc2
papertyga <>
2026-02-18 00:00:01 +0000

    Loop Control

---
+++ the_while_loop/loop_control/main.py
@@ -0,0 +1,14 @@
+# List of country names
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# List to hold selected countries
+selected = []
+
+#counters
+i = 0
+counter = 0
+    
+        
+
+# Testing
+print('First three countries starting with "S":', selected)
