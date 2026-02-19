beef1adddf7dc0fc3716bfa24d2a04ff9a5df019
papertyga <>
2026-02-19 22:31:58 +0000

    Nested loop printing

---
+++ nested_loops/nested_loop_printing/main.py
@@ -0,0 +1,19 @@
+# List of trips
+trips = [['france', 'germany', 'italy', 'spain', 'netherlands', 'sweden', 'norway', 'switzerland', 'austria', 'portugal', 'belgium'],
+         ['japan', 'china', 'thailand', 'vietnam', 'ndonesia', 'india', 'malaysia', 'philippines', 'singapore', 'mongolia'],
+         ['usa', 'canada', 'mexico', 'brazil', 'argentina', 'colombia', 'peru', 'chile', 'ecuador'],
+         ['egypt', 'morocco', 'south africa', 'tunisia', 'algeria', 'kenya', 'nigeria', 'ethiopia'],
+         ['australia', 'new zealand', 'fiji', 'papua new guinea', 'samoa']]
+
+# List of all countries 
+countries = []
+
+for trip in trips:
+    for country in trip:
+        countries.append(country.capitalize())
+        print(country.capitalize(), end=' ')
+    
+countries.sort()
+
+# Testing
+print('List of Countries:', countries)
